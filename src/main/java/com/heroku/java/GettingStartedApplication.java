35535e208f6adc20935abacd30783fcf77565228
2024421164 <2024421164@student.uitm.edu.my>
2026-06-16 16:04:27 +0800

    Clean repo: remove heroku template, keep only src, pom, readme, gitignore, and license

--- src/main/java/com/heroku/java/GettingStartedApplication.java
+++ src/main/java/com/heroku/java/GettingStartedApplication.java
@@ -1,19 +1,13 @@
 package com.heroku.java;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
 @RestController
-public class GettingStartedApplication {
-    @GetMapping("/")
-    public String index() {
-        return "App is Running!";
-    }
+public class HomeController {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GettingStartedApplication.class, args);
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World! Your Spring Boot app is running.";
     }
 }
